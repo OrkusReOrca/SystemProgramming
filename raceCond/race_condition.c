d5efe512d2d86aea2122536561c36ab91301a6f1
OrkusReOrca <146055263+OrkusReOrca@users.noreply.github.com>
2024-10-18 07:34:12 +0000

    hi

---
+++ raceCond/race_condition.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+pid_t pid; char *msg, c; int n;
+
+printf("fork program starting\n");
+pid = fork();
+switch(pid)
+{
+case -1: exit(1);
+case 0: msg = “C”; n = 35; break;
+default: msg = “P”; n = 35; break;
+}
+
+setbuf(STDOUT, NULL); // set unbuffered
+for (; n>0; n--)
+{
+while(c = *msg++)
+{putc(c, STDOUT); sleep(1);}
+}
+if (pid)
+{
+int stat_val; pid_t child_pid;
+child_pid = wait(&stat_val);
+printf("Child has nished: PID = %d\n",child_pid);
+if (WIFEXITED(stat_val)
+printf(“\"Child exited with code %d\n",WEXITSTATUS(stat_val);
+else
+printf("Child terminated abnormally\n");
+}
+exit(0);
+}
