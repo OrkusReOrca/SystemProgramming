a7ddbf8d561e44b72adb4c7d526d250018c8e054
OrkusReOrca <146055263+OrkusReOrca@users.noreply.github.com>
2024-10-28 08:55:21 +0000

    .

--- pipes/Rpipes.c
+++ pipes/Rpipes.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) //reader.c
+int main(int argc, char *argv[]) // reader.c
 {
-int n_bytes, fd;
-char buffer[BUFSIZ + 1];
+  int n_bytes, fd;
+  char buffer[BUFSIZ + 1];
 
-memset(buffer, '\0', sizeof(buffer));
-sscanf(argv[1], "%d", &fd);
-n_bytes = read(fd, buffer, BUFSIZ);
-printf("%d bytes have been read from %d : %s\n", n_bytes, getpid(), buffer);
-exit(EXIT_SUCCESS);
+  memset(buffer, '\0', sizeof(buffer));
+  sscanf(argv[1], "%d", &fd);
+  n_bytes = read(fd, buffer, BUFSIZ);
+  printf("%d bytes have been read from %d : %s\n", n_bytes, getpid(), buffer);
+  exit(EXIT_SUCCESS);
 }
